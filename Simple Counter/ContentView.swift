40478bcdc5f09e157ba56f12ee8f0259e47f532a
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2022-09-20 15:18:09 +0100

    Initial Commit

---
+++ Simple Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simple Counter
+//
+//  Created by Wing Hei Tsui on 20/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple Counter/Simple_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CounterApp.swift
+//  Simple Counter
+//
+//  Created by Wing Hei Tsui on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
